40cf29b6fd1058185f14283ba9f2aa3e1c3f5ae2
Murat <mzallov@yandex.ru>
2025-02-24 18:51:02 +0300

    week_03_roman

--- week_03/task_roman.py
+++ week_03/task_roman.py
@@ -1,11 +1,20 @@
 class Roman:
-
-    def __init__(self, arg_plus,arg_minus,arg_umn, arg_del):
+    def __init__(self, arg_plus,arg_minus,arg_umn, arg_del,roman_numbers):
         self.arg_plus = arg_plus
         self.arg_minus = arg_minus
         self.arg_umn = arg_umn
         self.arg_del = arg_del
+        self.roman_numbers = roman_numbers
 
+
+
+    def roma(self, number):
+        num = ''
+        for key, value in self.roman_numbers.items():
+            while number >= value:
+                num += key
+                number -= value
+        return num
 
     def __add__(self, other):
         return self.arg_plus + other
@@ -19,11 +28,18 @@ class Roman:
 
     def __truediv__(self, other):
         return self.arg_del / other
-a = Roman(3,3,4,5)
-print(a.__add__(4))
-print(a.__mul__(4))
-print(a.__sub__(4))
-print(a.__truediv__(4))
+roman = Roman(3,5,4,5,{'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
+                     'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
+                     'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1})
+plus = roman.__add__(4)
+minus = roman.__sub__(4)
+umnozhit = roman.__mul__(4)
+razdelit = roman.__truediv__(4)
+
+print(roman.roma(plus))
+print(roman.roma(minus))
+print(roman.roma(umnozhit))
+print(roman.roma(razdelit))
 
 
 
